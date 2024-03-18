a25888eb4b1761c830b04eff46a2ae9c6f571670
Tsotne <tsotne.tsirekidze11@gmail.com>
2024-03-18 20:27:50 +0400

    edited cors

--- index.php
+++ index.php
@@ -9,22 +9,23 @@ header("Content-type: application/json; charset=UTF-8");
 set_exception_handler("ErrorHandler::handleException");
 
 $method = $_SERVER['REQUEST_METHOD'];
-$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$dsn =  "mysql:host=127.0.0.1;dbname=Scandiweb Product DB;port=3306;charset=utf8";
-$db = new src\Database\DataBase($dsn,"root","3971");
+$dsn = "mysql:host=127.0.0.1;dbname=Scandiweb Product DB;port=3306;charset=utf8";
+$db = new src\Database\DataBase($dsn, "root", "3971");
 
 $productController = new src\product\productController($db);
 
-switch ($method){
+switch ($method) {
     case "GET":
-    $productController->handleGet($uri);
+
+        $productController->handleGet($uri);
         break;
     case "POST":
         $productController->handlePost($uri);
         break;
     case "DELETE":
-       $productController->handleDelete($uri);
+        $productController->handleDelete($uri);
         break;
     default:
         http_response_code(405);
--- src/Utils/cors.php
+++ src/Utils/cors.php
@@ -1,4 +1,14 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, DELETE");
+    header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization");
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization");
+
+
